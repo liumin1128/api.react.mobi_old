e7e20ef1f691dd7322785cf383cd7c05412f9d06
liumin1128 <970568830@qq.com>
2017-11-04 20:31:04 +0800

    更新

--- server/common/index.js
+++ server/common/index.js
@@ -11,25 +11,21 @@ class CommonController {
   async verifyPhone(ctx) {
     try {
       const code = randomCode();
-      console.log('code2');
-      console.log(code);
-      // const { phone } = ctx.request.body;
-      // const key = `p${phone}c${code}`;
-      // const expire = 5 * 60;
-      // const data = await sentSMS(phone, code);
-      // console.log('data');
-      // console.log(data);
-      // if (data && data.Code === 'OK') {
-      //   await setAsync(key, code, 'EX', expire);
-      //   ctx.body = {
-      //     status: 'ok',
-      //     message: `验证码已发送到：${phone}, ${expire}秒有效`,
-      //     expire,
-      //   };
-      // } else {
-      //   console.log(data);
-      //   ctx.throw(403, '验证码发送失败');
-      // }
+      const { phone } = ctx.request.body;
+      const key = `p${phone}c${code}`;
+      const expire = 5 * 60;
+      const data = await sentSMS(phone, code);
+      if (data && data.Code === 'OK') {
+        await setAsync(key, code, 'EX', expire);
+        ctx.body = {
+          status: 'ok',
+          message: `验证码已发送到：${phone}, ${expire}秒有效`,
+          expire,
+        };
+      } else {
+        console.log(data);
+        ctx.throw(403, '验证码发送失败');
+      }
     } catch (error) {
       ctx.throw(403, '验证码发送失败');
     }
