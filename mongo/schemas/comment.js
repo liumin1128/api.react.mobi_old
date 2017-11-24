51ec17da2cf2c17dee1e98fc97ca4ec1200e5f33
liumin1128 <970568830@qq.com>
2017-11-24 19:07:40 +0800

    更新

--- mongo/schemas/comment.js
+++ mongo/schemas/comment.js
@@ -6,7 +6,6 @@ export default {
   content: String,
   id: ObjectId,
   user: { type: ObjectId, ref: 'User' },
-  replyTo: { type: ObjectId, ref: 'Comment' },
   likes: Number,
   replies: Number,
 };
