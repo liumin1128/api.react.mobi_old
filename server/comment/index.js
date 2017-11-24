8eaa68bb8640ead3fbcffb69b9461b8555a48c15
liumin1128 <970568830@qq.com>
2017-11-24 09:42:27 +0800

    更新

--- server/comment/index.js
+++ server/comment/index.js
@@ -37,7 +37,7 @@ class CommentController {
     console.log(count);
 
     const list = await Comment.find(params)
-      .ne('replyTo')
+      .where('replyTo').ne()
       .skip((page === 0 ? page : page - 1) * pageSize)
       .populate('user', POPULATE_USER)
       .populate('replyTo')
