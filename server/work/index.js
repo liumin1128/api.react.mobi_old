86292635efd22828fad57d92d257a9e1381d1e61
liumin1128 <970568830@qq.com>
2017-12-24 17:06:01 +0800

    微信签名

--- server/work/index.js
+++ server/work/index.js
@@ -4,6 +4,7 @@ import work from './server';
 const router = new Router();
 
 export default router
+  .post('/getJsSdkConfig', work.getJsSdkConfig)
   .get('/mlogin', work.mlogin)
   .get('/mcallback', work.mcallback)
   .get('/pclogin', work.pclogin)
--- server/work/server.js
+++ server/work/server.js
@@ -1,7 +1,10 @@
 import moment from 'moment';
+import crypto from 'crypto';
+import { parse, stringify } from 'query-string';
 import request from '../../utils/fetch';
 import { CORPID, CORPSECRET_HUARENHOUSE, REDIRECT_URI } from '../../config/work';
 import { getAsync, setAsync, delAsync } from '../../utils/redis';
+import { randomString } from '../../utils/common';
 
 async function getAccessToken() {
   try {
@@ -46,6 +49,15 @@ async function getUserDetailInfo({ token, user_ticket }) {
     const url = `https://qyapi.weixin.qq.com/cgi-bin/user/getuserdetail?access_token=${token}`;
     const params = { user_ticket };
     return await request(url, params);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getJsApiTicket({ token }) {
+  try {
+    const url = `https://qyapi.weixin.qq.com/cgi-bin/get_jsapi_ticket?access_token=${token}`;
+    return await request(url);
   } catch (error) {
     console.log(error);
   }
@@ -101,7 +113,53 @@ class Work {
       });
     }
   }
+  async getJsSdkConfig(ctx) {
+    try {
+      const { url } = ctx.request.body;
+      console.log('url');
+      console.log(url);
+      const nonceStr = randomString();
+      const timestamp = parseInt(moment().format('X'), 0);
+      const token = await getAccessToken();
+      const { errmsg, ticket: jsapi_ticket } = await getJsApiTicket({ token });
+      if (errmsg === 'ok') {
+        const temp = stringify({
+          noncestr: nonceStr, jsapi_ticket, timestamp, url,
+        });
+        console.log('temp');
+        console.log(temp);
 
+        const signature = crypto
+          .createHash('sha1')
+          .update(temp)
+          .digest('hex');
+
+        ctx.body = JSON.stringify({
+          beta: true, // 必须这么写，否则在微信插件有些jsapi会有问题
+          debug: true, // 开启调试模式
+          appId: CORPID, // 必填，企业微信的corpID
+          timestamp, // 必填，生成签名的时间戳
+          nonceStr, // 必填，生成签名的随机串
+          signature, // 必填，签名，见[附录1](#11974)
+          jsApiList: ['getLocation'], // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        });
+      }
+    } catch (error) {
+      console.log('getJsSdkConfig error');
+      console.log(error);
+    }
+
+
+    // ctx.body = JSON.stringify({
+    //   beta: true, // 必须这么写，否则在微信插件有些jsapi会有问题
+    //   debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    //   appId: CORPID, // 必填，企业微信的corpID
+    //   timestamp, // 必填，生成签名的时间戳
+    //   nonceStr, // 必填，生成签名的随机串
+    //   signature: '', // 必填，签名，见[附录1](#11974)
+    //   jsApiList: [], // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+    // });
+  }
   async test(ctx) {
     try {
       const token = await getAccessToken();
