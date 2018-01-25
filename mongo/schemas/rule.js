dedf5d2650d7c936f0937e8a5074b28a019ea908
liumin1128 <970568830@qq.com>
2018-01-25 17:32:47 +0800

    更新

--- mongo/schemas/rule.js
+++ mongo/schemas/rule.js
@@ -4,8 +4,8 @@ const { ObjectId } = mongoose.Schema.Types;
 
 export default {
   name: String, // 打卡规则名
-  times: [Number],
-  standard: [Number],
+  times: [Number], // 规则适用时间段
+  standard: [Number], // 标准时间段，若未打卡，则按标准时间计算
   user: { type: ObjectId, ref: 'User' },
   location: Object,
   networkType: String, // 网络类型，前端会要求为wifi
