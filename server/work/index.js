3437e6ff74f2b2ea9ca908c8db424b1a05854073
liumin1128 <970568830@qq.com>
2018-01-30 11:27:37 +0800

    更新

--- server/work/index.js
+++ server/work/index.js
@@ -235,7 +235,7 @@ class Work {
 
       const data = await Daka
         .find(params)
-        .where('likes').in(rule)
+        .where('rule').in(rule)
         .gte('createdAt', starttime)
         .lte('createdAt', endtime)
         .populate('user')
