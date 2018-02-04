feba838509a95fe821f869eba23f2ccac2496d84
liumin1128 <970568830@qq.com>
2018-02-04 09:42:20 +0800

    gx1

--- auto_build.sh
+++ auto_build.sh
@@ -2,8 +2,8 @@ PRO_DIR="../api.react.mobi"
 echo "start--------------------"
 cd $PRO_DIR
 source ~/.nvm/nvm.sh
-nvm use 8.9.0
-echo "切换到8.9.0"
+# nvm use 8.9.0
+# echo "切换到8.9.0"
 echo "Node 版本："
 node -v
 echo "cd $PRO_DIR"
