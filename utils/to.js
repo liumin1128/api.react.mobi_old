a8746135e5ec6f0e918c40e06da3c2efcec3ca42
liumin1128 <970568830@qq.com>
2018-02-14 08:39:34 +0800

    gx

---
+++ utils/to.js
@@ -0,0 +1,28 @@
+// to.js
+export default function to(promise) {
+  return promise.then((data) => {
+    return [null, data];
+  })
+    .catch(err => [err]);
+}
+
+import to from './to.js';
+
+async function asyncTask(cb) {
+  let err, 
+user, 
+savedTask;
+
+  [err, user] = await to(UserModel.findById(1));
+  if (!user) return cb('No user found');
+
+  [err, savedTask] = await to(TaskModel({ userId: user.id, name: 'Demo Task' }));
+  if (err) return cb('Error occurred while saving task');
+
+  if (user.notificationsEnabled) {
+    const [err] = await to(NotificationService.sendNotification(user.id, 'Task Created'));
+    if (err) return cb('Error while sending notification');
+  }
+
+  cb(null, savedTask);
+}
