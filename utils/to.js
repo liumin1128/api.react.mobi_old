f927b6c4c221cfd74f11bc896f6e66a0e504484b
liumin1128 <970568830@qq.com>
2018-03-02 11:15:46 +0800

    更新

--- utils/to.js
+++ utils/to.js
@@ -1,17 +1,17 @@
 // to.js
-export default function to(promise) {
-  return promise.then((data) => {
-    return [null, data];
-  })
-    .catch(err => [err]);
-}
+// export default function to(promise) {
+//   return promise.then((data) => {
+//     return [null, data];
+//   })
+//     .catch(err => [err]);
+// }
 
 import to from './to.js';
 
 async function asyncTask(cb) {
-  let err, 
-user, 
-savedTask;
+  let err,
+    user,
+    savedTask;
 
   [err, user] = await to(UserModel.findById(1));
   if (!user) return cb('No user found');
