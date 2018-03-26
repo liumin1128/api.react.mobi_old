f70cddd17e479d0af8794c2a85fad93d16555349
liumin1128 <970568830@qq.com>
2018-03-26 20:11:04 +0800

    更新

--- graphql/index.js
+++ graphql/index.js
@@ -3,29 +3,6 @@ import { makeExecutableSchema } from 'graphql-tools';
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
-// // Some fake data
-// const books = [
-//   {
-//     title: "Harry Potter and the Sorcerer's stone",
-//     author: 'J.K. Rowling',
-//   },
-//   {
-//     title: 'Jurassic Park',
-//     author: 'Michael Crichton',
-//   },
-// ];
-
-// // The GraphQL schema in string form
-// const typeDefs = `
-//   type Query { books: [Book] }
-//   type Book { title: String, author: String }
-// `;
-
-// // The resolvers
-// const resolvers = {
-//   Query: { books: () => books },
-// };
-
 // Put together a schema
 const schema = makeExecutableSchema({
   typeDefs,
--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -40,10 +40,8 @@ export default {
   Say: {
     user: async ({ user }) => {
       if (user._id) {
-        // console.log('已存在user字段');
         return user;
       }
-      // console.log(`user字段为：${user}`);
       const data = await User.findById(user);
       return data;
     },
