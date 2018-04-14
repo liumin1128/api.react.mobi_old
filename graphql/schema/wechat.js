2066a90cfeb7b62bea1496e31e56d58c1479ff28
liumin1128 <970568830@qq.com>
2018-04-15 02:02:43 +0800

    刚刚

--- graphql/schema/wechat.js
+++ graphql/schema/wechat.js
@@ -1,8 +1,8 @@
 export default `
   type Wechat {
-    title: String!
-    createdAt: String!
-    url: String!
-    cover: String!
+    title: String
+    createdAt: String
+    url: String
+    cover: String
   }
 `;
