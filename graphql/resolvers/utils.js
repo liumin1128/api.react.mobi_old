a4202836e66e8ffaf8272d357b58ed8550df392c
liumin1128 <970568830@qq.com>
2018-04-15 01:17:25 +0800

    更新

---
+++ graphql/resolvers/utils.js
@@ -0,0 +1,15 @@
+import DataLoader from 'dataloader';
+import uniq from 'lodash/uniq';
+import { User } from '../../mongo/modals';
+
+export const userLoader = new DataLoader(ids => User
+  .find({ _id: { $in: uniq(ids) } })
+  .then(data => ids.map(id => data.find(i => `${i._id}` === `${id}`))));
+
+export const resolverCombine = (...args) => {
+  return {
+    ...args.map(({ Mutation, Query, ...other }) => other),
+    Mutation: { ...args.map(({ Mutation }) => Mutation) },
+    Query: { ...args.map(({ Query }) => Query) },
+  };
+};
