3dff22839954bb2a5c177b560630e2eb95785f1d
liumin1128 <970568830@qq.com>
2018-04-15 23:17:04 +0800

    更新

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -3,7 +3,8 @@ import article from './models/article/resolver';
 import wechat from './models/wechat/resolver';
 import meizitu from './models/meizitu/resolver';
 import mzitu from './models/mzitu/resolver';
+import bxgif from './models/bxgif/resolver';
 
 import { resolverCombine } from './utils';
 
-export default resolverCombine(say, article, wechat, meizitu, mzitu);
+export default resolverCombine(say, article, wechat, meizitu, mzitu, bxgif);
