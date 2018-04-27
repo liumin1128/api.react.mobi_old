d64192e00442ed396054bb725d22e16bf56f17cc
liumin1128 <970568830@qq.com>
2018-04-27 09:30:51 +0800

    更新

--- graphql/models/user/resolver.js
+++ graphql/models/user/resolver.js
@@ -7,8 +7,12 @@ export default {
       try {
         const { password, ...other } = args;
         const user = await User.findOne(other);
+        console.log('user');
+        console.log(user);
         if (user && `${password}` === user.password) {
           const token = await getUserToken(user._id);
+          console.log('token');
+          console.log(token);
           return {
             status: 200,
             token,
