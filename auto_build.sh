f79472ce201a5b31e21db765a6114027d23d5486
liumin1128 <970568830@qq.com>
2018-04-27 09:26:37 +0800

    更新

--- auto_build.sh
+++ auto_build.sh
@@ -14,14 +14,14 @@ echo "从 $PATH_OLD 切换到 $PATH_NEW"
 # echo "cd $PRO_DIR"
 
 echo "正在从git同步代码"
-echo "git fetch --all"
-echo "git reset --hard origin/master"
-echo "git pull"
+git fetch --all
+git reset --hard origin/master
+git pull
 
 echo "安装依赖"
-echo "yarn"
+yarn
 
 echo "重启pm2"
 # pm2 restart api.react.mobi
-echo "yarn pm2"
+yarn pm2
 echo "finished----------"
