b688a2cd690d54a35782e4173f144d42d536a70e
liumin1128 <970568830@qq.com>
2018-05-03 22:50:32 +0800

    更新

--- graphql/models/other/resolver.js
+++ graphql/models/other/resolver.js
@@ -2,7 +2,7 @@ import { todayInHistory } from '../../../utils/showapi';
 
 export default {
   Query: {
-    mzituList: async (root, { date = '1128' }) => {
+    todayInHistory: async (root, { date = '1128' }) => {
       const list = await todayInHistory(date);
       console.log('list');
       console.log(list);
