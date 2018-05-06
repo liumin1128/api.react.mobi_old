7020581c42a06a26086b4fc851fae72971730a04
liumin1128 <970568830@qq.com>
2018-05-06 20:41:24 +0800

    更新

--- graphql/models/article/schema.js
+++ graphql/models/article/schema.js
@@ -10,9 +10,15 @@ export default `
     createdAt: String!
     _id: String!
   }
+  type ArticleType {
+    tag: String!
+  }
   input ArticleInput {
     title: String!
     content: String!
     cover: String
+    rawData: String
+    rawDataType: String
+    tags: [ArticleType]
   }
 `;
--- mongo/schemas/article.js
+++ mongo/schemas/article.js
@@ -9,4 +9,8 @@ export default {
   description: String,
   tags: Array,
   user: { type: ObjectId, ref: 'User' },
+  // 原始数据
+  rawData: String,
+  // 原始数据类型
+  rawDataType: String,
 };
