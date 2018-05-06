90c1805009706b628b2f6cbeb29cfa8cd99a8535
liumin1128 <970568830@qq.com>
2018-05-06 20:47:01 +0800

    更新

--- graphql/models/article/schema.js
+++ graphql/models/article/schema.js
@@ -10,15 +10,12 @@ export default `
     createdAt: String!
     _id: String!
   }
-  type ArticleType {
-    tag: String!
-  }
   input ArticleInput {
     title: String!
     content: String!
     cover: String
     rawData: String
     rawDataType: String
-    tags: [ArticleType]
+    tags: [String]
   }
 `;
