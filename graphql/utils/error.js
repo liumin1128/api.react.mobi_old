facbea4623b6f5e53cfc44cecdb62ac11223a02d
liumin1128 <970568830@qq.com>
2018-05-07 21:49:13 +0800

    更新

--- graphql/utils/error.js
+++ graphql/utils/error.js
@@ -1,17 +1,1 @@
-import { createError } from 'apollo-errors';
 
-export const throwError = (options) => {
-  const {
-    type = 'FooError',
-    message = 'A foo error has occurred',
-    data = { something: 'important' },
-  } = options;
-
-  const FooError = createError(type, { message });
-  throw new FooError({
-    data,
-    internalData: {
-      error: 'The SQL server died.',
-    },
-  });
-};
