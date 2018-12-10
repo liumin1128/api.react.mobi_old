5ecd9ed5b19d36ff40a2d38d4fcdb734d6f5ebef
liumin1128 <970568830@qq.com>
2018-12-10 20:38:57 +0800

    更新

--- server/oauth/weibo.js
+++ server/oauth/weibo.js
@@ -47,7 +47,9 @@ class Weibo {
       // // 如果不存在则创建新用户，并保存该用户的第三方登录信息
       // if (!oauth) {
       // 获取用户信息
-      const userinfo = await fetch(`https://api.weibo.com/oauth2/get_token_info?access_token=${access_token}`);
+      const userinfo = await fetch(`https://api.weibo.com/2/users/show.json?access_token=${access_token}&uid=${uid}`, {}, { method: 'GET' });
+      // const userinfo = await fetch(`https://api.weibo.com/oauth2/get_token_info?access_token=${access_token}`);
+
       console.log('userinfo------');
       console.log(userinfo);
       ctx.redirect(DOMAIN);
