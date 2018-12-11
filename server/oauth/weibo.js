36114f711b57dfda2d4bc06da591b8e9df001808
liumin1128 <970568830@qq.com>
2018-12-11 22:55:15 +0800

    更新微博登录逻辑

--- server/oauth/weibo.js
+++ server/oauth/weibo.js
@@ -6,60 +6,76 @@ import request from '@/utils/fetch';
 import { fetchToQiniu } from '@/utils/qiniu';
 import { getUserToken } from '@/utils/jwt';
 
+function getOauthUrl() {
+  let url = 'https://api.weibo.com/oauth2/authorize';
+  url += `?client_id=${weibo.App_Key}`;
+  url += `&redirect_uri=${weibo.redirect_uri}`;
+  return url;
+}
+
+async function getAccessToken(code) {
+  try {
+    let url = 'https://api.weibo.com/oauth2/access_token';
+    url += `?client_id=${weibo.App_Key}`;
+    url += `&client_secret=${weibo.App_Secret}`;
+    url += `&code=${code}`;
+    url += '&grant_type=authorization_code';
+    url += `&redirect_uri=${weibo.redirect_uri}`;
+
+    const data = await request(url);
+
+    return data;
+  } catch (error) {
+    console.log('error');
+    console.log(error);
+  }
+}
+
+async function getUserInfo(access_token, uid) {
+  try {
+    const data = await fetch(`https://api.weibo.com/2/users/show.json?access_token=${access_token}&uid=${uid}`, { method: 'GET' })
+      .then((res) => {
+        return res.json();
+      });
+    return data;
+  } catch (error) {
+    console.log('error');
+    console.log(error);
+  }
+}
+
 class Weibo {
   // 用户注册
   async login(ctx) {
-    console.log('新用户登录');
-    // const dataStr = (new Date()).valueOf();
-    // 重定向到认证接口,并配置参数
-
-    let path = 'https://api.weibo.com/oauth2/authorize';
-    path += `?client_id=${weibo.App_Key}`;
-    path += `&redirect_uri=${weibo.redirect_uri}`;
-    // path += '&response_type=code&scope=snsapi_login&state=123#weibo_redirect ';
-    // 转发到授权服务器
-    ctx.redirect(path);
+    console.log('微博用户登录');
+    ctx.redirect(getOauthUrl());
   }
 
   async callback(ctx) {
     try {
       const { code } = ctx.query;
 
-      // console.log('code');
-      // console.log(code);
-      // 用token换取access_token
-      let au = 'https://api.weibo.com/oauth2/access_token';
-      au += `?client_id=${weibo.App_Key}`;
-      au += `&client_secret=${weibo.App_Secret}`;
-      au += `&code=${code}`;
-      au += '&grant_type=authorization_code';
-      au += `&redirect_uri=${weibo.redirect_uri}`;
-
-      const { access_token, uid } = await request(au);
+      const data = await getAccessToken(code);
+      const { access_token, uid } = data;
       if (!access_token) {
         ctx.redirect(DOMAIN);
       }
+
       // 从数据库查找对应用户第三方登录信息
       let oauth = await Oauth.findOne({ from: 'weibo', 'data.uid': uid });
 
       // 如果不存在则创建新用户，并保存该用户的第三方登录信息
-      if (!oauth) {
+      if (oauth) {
+        // 更新三方登录信息
+        oauth.update({ data });
+      } else {
         // 获取用户信息
-        const userinfo = await fetch(`https://api.weibo.com/2/users/show.json?access_token=${access_token}&uid=${uid}`, { method: 'GET' })
-          .then((res) => {
-            return res.json();
-          });
-
-        const { name: nickname, profile_image_url } = userinfo;
-
-        // console.log('profile_image_url');
-        // console.log(profile_image_url);
+        const userInfo = await getUserInfo(access_token, uid);
+        const { name: nickname, profile_image_url } = userInfo;
         // // 将用户头像上传至七牛
         const avatarUrl = await fetchToQiniu(profile_image_url);
-        // console.log(avatarUrl);
         const user = await User.create({ avatarUrl, nickname });
-        // await client.setAsync(user._id, user);
-        oauth = await Oauth.create({ from: 'weibo', data: { ...userinfo, access_token, uid }, user });
+        oauth = await Oauth.create({ from: 'weibo', data, userInfo, user });
       }
       // 生成token（用户身份令牌）
       const token = await getUserToken(oauth.user);
