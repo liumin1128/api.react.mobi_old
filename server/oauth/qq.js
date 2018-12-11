48d6cb6704697fa2c07746601f9dd7ef6bf7eae8
liumin1128 <970568830@qq.com>
2018-12-11 23:01:10 +0800

    更新

--- server/oauth/qq.js
+++ server/oauth/qq.js
@@ -133,6 +133,8 @@ class Qq {
 
       if (oauth) {
         // 更新三方登录信息
+        console.log('更新三方登录信息');
+        console.log(data);
         oauth.update({ data });
       } else {
         // 如果不存在则获取用户信息，创建新用户，并保存该用户的第三方登录信息
