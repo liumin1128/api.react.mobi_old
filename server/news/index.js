6c1d8a1b30af45be1b16c44640038f0e24e91576
liumin1128 <970568830@qq.com>
2019-04-13 07:46:43 +0800

    更新

--- server/news/index.js
+++ server/news/index.js
@@ -4,10 +4,9 @@ import { getData } from './service';
 
 // 今日头条，网易新闻， sohu.com的图片显示有问题，只能用文本模式
 
-
 function test() {
-  // const start = moment().subtract(10, 'minute');
-  const start = moment().subtract(2, 'day');
+  const start = moment().subtract(10, 'minute');
+  // const start = moment().subtract(2, 'day');
   const end = moment();
   const publishDateRange = `${start.format('X')},${end.format('X')}`;
   console.log(`抓取${start.format('llll')}至今的文章`);
@@ -20,13 +19,21 @@ function test() {
 }
 
 
-test();
+// test();
 
 // switch,cos
 
 /* eslint-disable no-new */
-// new CronJob('0 */10 * * * *', () => {
-//   console.log(`10分钟抓取一次，${moment().format('llll')}`);
-//   test();
-// }, null, true);
+new CronJob('0 */10 * * * *', () => {
+  console.log(`10分钟抓取一次，${moment().format('llll')}`);
+  const start = moment().subtract(10, 'minute');
+  const end = moment();
+  const publishDateRange = `${start.format('X')},${end.format('X')}`;
+  console.log(`抓取${start.format('llll')}至今的文章`);
+  getData({
+    keyword: 'switch',
+    catLabel1: '游戏',
+    publishDateRange,
+  });
+}, null, true);
 /* eslint-enable no-new */
