bdb25103006d10ea6dd4863d994debcb20b73ffc
liumin1128 <970568830@qq.com>
2019-06-30 13:56:44 +0800

    更新

--- server/news/index.js
+++ server/news/index.js
@@ -1,40 +1,25 @@
 import moment from 'moment';
 import { CronJob } from 'cron';
-import { isProd } from '@/config/base';
+import { ENV } from '@/config/base';
 import { getData } from './service';
 
 // 今日头条，网易新闻， sohu.com的图片显示有问题，只能用文本模式
 
-function test() {
-  const start = moment().subtract(1, 'day');
-  const end = moment();
-  const publishDateRange = `${start.format('X')},${end.format('X')}`;
-  console.log(`抓取${start.format('llll')}至今的文章`);
-  getData({
-    keyword: 'switch',
-    catLabel1: '游戏',
-    publishDateRange,
-  });
-}
-
-
 // test();
 // switch,cos
 
-if (isProd) {
+if (!ENV) {
 /* eslint-disable no-new */
   new CronJob('0 */60 * * * *', () => {
-    test();
-  // console.log(`10分钟抓取一次，${moment().format('llll')}`);
-  // const start = moment().subtract(10, 'minute');
-  // const end = moment();
-  // const publishDateRange = `${start.format('X')},${end.format('X')}`;
-  // console.log(`抓取${start.format('llll')}至今的文章`);
-  // getData({
-  //   keyword: 'switch',
-  //   catLabel1: '游戏',
-  //   publishDateRange,
-  // });
+    const start = moment().subtract(1, 'day');
+    const end = moment();
+    const publishDateRange = `${start.format('X')},${end.format('X')}`;
+    console.log(`抓取${start.format('llll')}至今的文章`);
+    getData({
+      keyword: 'switch',
+      catLabel1: '游戏',
+      publishDateRange,
+    });
   }, null, true);
   /* eslint-enable no-new */
 }
